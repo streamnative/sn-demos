594609b82a8bc8a9a18b1b33edfdc500ae05c31f
ipolyzos <ipolyzos.se@gmail.com>
2022-01-31 09:01:15 +0200

    add-pulsar-to-bigquery-demo

--- pulsar-to-bigquery/src/main/java/io/streamnative/config/AppConfig.java
+++ pulsar-to-bigquery/src/main/java/io/streamnative/config/AppConfig.java
@@ -1,9 +1,7 @@
 package io.streamnative.config;
 
 public class AppConfig {
-    public static final String SERVICE_HTTP_URL = "http://localhost:8080";
     public static final String SERVICE_URL ="pulsar://localhost:6650";
-    public static final String STATE_STORAGE_SERVICE_URL    = "bk://localhost:4181";
 
     public static final String RAW_EVENTS_TOPIC         = "raw_events";
     public static final String PARSED_EVENTS_TOPIC      = "parsed_events";
